ad0f3686f10d8ae14e059e81f5d6d50374c12b39
eliasantoniorodrigues1 <eliasantoniorodrigues1@gmail.com>
2024-03-17 16:59:37 -0300

    add class material

--- api/main.py
+++ api/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(title='Fetal Health API',
+              openapi_tags=[
+                  {
+                  'name': '',
+                  'description': '',
+                  },
+                  {
+                    'name': '',
+                    'description': ''
+                    }])
 
+
+# to run fasapi: uvicorn api.main:app -- reload
 @app.get(path='/')
 def home():
     return {'status': 'healthy'}
